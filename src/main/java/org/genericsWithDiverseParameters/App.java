4c121a825495e1b697271423eeab9b259ae7380c
adriElias <34091624+adriElias@users.noreply.github.com>
2026-03-01 00:42:24 +0100

    feat: add partial generic method with fixed String parameter

--- src/main/java/org/genericsWithDiverseParameters/App.java
+++ src/main/java/org/genericsWithDiverseParameters/App.java
@@ -1,16 +1,19 @@
 package org.genericsWithDiverseParameters;
 
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "--- GENERICS ---" );
+public class App {
+    public static void main(String[] args) {
+        System.out.println("--- GENERICS ---");
         GenericMethods genericMethods = new GenericMethods();
         Person person = new Person("Adriana", "Villarroel", 27);
         String city = "Bolivia";
         int birthdate = 1998;
 
-        genericMethods.printElements(person,city, birthdate);
+        genericMethods.printElements(person, city, birthdate);
+
+        System.out.println("--- Level 2, exercise 1 ---");
+
+        genericMethods.printElementsPartialGeneric(100, "Hello", true);
+        genericMethods.printElementsPartialGeneric(3.14, "Madrid", person);
 
     }
 }
--- src/main/java/org/genericsWithDiverseParameters/GenericMethods.java
+++ src/main/java/org/genericsWithDiverseParameters/GenericMethods.java
@@ -1,9 +1,15 @@
 package org.genericsWithDiverseParameters;
 
 public class GenericMethods {
-    public <T,U,V> void printElements(T element1, U element2, V element3){
+    public <T, U, V> void printElements(T element1, U element2, V element3) {
         System.out.println("Element 1: " + element1);
         System.out.println("Element 2: " + element2);
+        System.out.println("Element 3: " + element3);
+    }
+
+    public <T, V> void printElementsPartialGeneric(T element1, String element2, V element3) {
+        System.out.println("Element 1: " + element1);
+        System.out.println("Element 2:" + element2);
         System.out.println("Element 3: " + element3);
     }
 }
